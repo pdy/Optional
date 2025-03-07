803a9ce3d5ae83cca8a0f07c3c54ef814915dd23
pdy <pdrzycimski@gmail.com>
2025-03-07 23:02:30 +0100

    Add more UTs

--- tests/Optional_20_UT.cpp
+++ tests/Optional_20_UT.cpp
@@ -27,6 +27,9 @@
 #include <Optional.hpp>
 #include <type_traits>
 
+#include <cstddef>
+#include <cstdint>
+
 namespace {
 
 template<typename T>
@@ -38,7 +41,8 @@ constexpr bool size_check()
 template<typename T>
 constexpr bool has_noexcept_swap()
 {
-  return noexcept(swap(std::declval<T&>(), std::declval<T&>()));
+  using NonConst_T = std::remove_const<T>;
+  return noexcept(swap(std::declval<NonConst_T&>(), std::declval<NonConst_T&>()));
 }
 
 } // namespace
@@ -51,7 +55,7 @@ TYPED_TEST_SUITE_P(OptionalArithmeticTests);
 
 TYPED_TEST_P(OptionalArithmeticTests, emptyTest)
 {
-  Optional<TypeParam> empty;
+  const Optional<TypeParam> empty;
 
   EXPECT_FALSE(empty);
   EXPECT_TRUE(!empty);
@@ -87,7 +91,7 @@ TYPED_TEST_P(OptionalArithmeticTests, ctorAndReset)
 
 TYPED_TEST_P(OptionalArithmeticTests, assignValue)
 {
-  Optional<TypeParam> val = TypeParam{10};
+  const Optional<TypeParam> val = TypeParam{10};
 
   EXPECT_TRUE(val);
   EXPECT_FALSE(!val);
@@ -106,14 +110,44 @@ TYPED_TEST_P(OptionalArithmeticTests, assingToEmpty)
   EXPECT_EQ(TypeParam{10}, val.value_or(TypeParam{5}));
 }
 
-REGISTER_TYPED_TEST_SUITE_P(OptionalArithmeticTests, emptyTest, ctorAndReset, assignValue, assingToEmpty);
+TYPED_TEST_P(OptionalArithmeticTests, returnFromCallable)
+{
+  const auto callable = []() -> Optional<TypeParam> { return TypeParam{10}; };
+  const auto callable_2 = []() -> Optional<TypeParam> { return Optional<TypeParam>{TypeParam{10}}; };
+
+  const auto val = callable();
+  const auto val_2 = callable_2();
+
+  EXPECT_TRUE(val);
+  EXPECT_FALSE(!val);
+  EXPECT_TRUE(val.has_value());
+  EXPECT_EQ(TypeParam{10}, val.value_or(TypeParam{5}));
+  EXPECT_TRUE(size_check<TypeParam>());
+  EXPECT_TRUE(has_noexcept_swap<decltype(val)>());
+  EXPECT_TRUE(std::is_trivially_destructible_v<decltype(val)>);
+
+  EXPECT_TRUE(val_2);
+  EXPECT_FALSE(!val_2);
+  EXPECT_TRUE(val_2.has_value());
+  EXPECT_EQ(TypeParam{10}, val_2.value_or(TypeParam{5}));
+  EXPECT_TRUE(has_noexcept_swap<decltype(val_2)>());
+  EXPECT_TRUE(std::is_trivially_destructible_v<decltype(val_2)>);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(OptionalArithmeticTests, emptyTest, ctorAndReset, assignValue, assingToEmpty, returnFromCallable);
 
 using TestTypes = ::testing::Types<
   char, signed char, unsigned char,
   short, short int, signed short, signed short int, unsigned short, unsigned short int,
   int, signed, signed int, unsigned, unsigned int,
   long, long int, signed long, signed long int, long long, long long int, signed long long, signed long long int, unsigned long long, unsigned long long int,
-  float, double, long double
+  float, double, long double,
+  // cstddef
+  size_t, ptrdiff_t, 
+  // cstdint
+  int8_t, int16_t, int32_t, int64_t, int_fast8_t, int_fast16_t, int_fast32_t, int_fast64_t, int_least8_t, int_least16_t, int_least32_t, int_least64_t,
+  uint8_t, uint16_t, uint32_t, uint64_t, uint_fast8_t, uint_fast16_t, uint_fast32_t, uint_fast64_t, uint_least8_t, uint_least16_t, uint_least32_t, uint_least64_t,
+  intmax_t, intptr_t, uintmax_t, uintptr_t
 >;
-
+ 
 INSTANTIATE_TYPED_TEST_SUITE_P(My, OptionalArithmeticTests, TestTypes);
